e83e0cc30bc19b1641a53d484a1ec595a7463d45
Goat <ryantgray@gmail.com>
2019-11-11 12:47:42 -0600

    Add BackchannelMessage Event that broadcasts to back_channel

---
+++ app/Events/BackchannelMessage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class BackchannelMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($message = 'no message')
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('back_channel');
+    }
+
+    /**
+     * Get the tags that should be assigned to the event.
+     *
+     * @return array
+     */
+    public function tags()
+    {
+        return ['backchannel'];
+    }
+}
