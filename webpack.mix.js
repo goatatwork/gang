75aef5a4d7749dd1c2396523194948a0ba764953
Goat <ryantgray@gmail.com>
2019-12-07 11:24:41 -0600

    Add trix to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+   .js('resources/js/trix.js', 'public/js/trix.js')
+   .styles([
+        'resources/css/trix.css'
+    ], 'public/css/trix.css')
    .sass('resources/sass/app.scss', 'public/css');
