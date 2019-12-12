7b110d66133c62975b5c1d11979b6d37c3484580
Goat <ryantgray@gmail.com>
2019-12-12 13:50:57 -0600

    Remove use of modals from dnsmasq page - replace mostly with collapse

--- resources/views/dnsmasq/index.blade.php
+++ resources/views/dnsmasq/index.blade.php
@@ -36,26 +36,87 @@
                         </li>
 
                         <li class="list-group-item">
-                            <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#server-config-file-modal">view config file</button>
-                            <a href="{{route('files.index')}}?load=dhcp_configs/dnsmasq.conf" class="btn btn-sm btn-warning">edit config file</a>
-                            @include('dnsmasq._server_config_file_modal')
+                            <span class="fas fa-cog" style="font-size:1em;"></span>
+                            <button data-target="#collapsalbe-view-of-dnsmasq-conf-file"
+                                data-toggle="collapse"
+                                class="btn btn-sm btn-success"
+                            >
+                                <span class="fas fa-eye"></span>
+                                View the dnsmasq.conf file
+                            </button>
+
+                            OR
+
+                            <a href="{{route('files.index')}}?load=dhcp_configs/dnsmasq.conf" class="btn btn-sm btn-primary">
+                                Edit the dnsmasq.conf file
+                            </a>
+
+                            <div id="collapsalbe-view-of-dnsmasq-conf-file" class="row collapse justify-content-center">
+                                <div class="col-11 mt-5">
+<small><pre>
+{{ $server_config_file }}
+</pre></small>
+                                </div>
+                            </div>
+
                         </li>
 
                         <li class="list-group-item">
-                            <button class="btn btn-sm btn-info" data-toggle="modal" data-target="#dnsmasq-leases-modal">Leases</button>
+                            <span class="fas fa-tags" style="font-size:1em;"></span>
+                            <button class="btn btn-sm btn-info" data-toggle="modal" data-target="#dnsmasq-leases-modal">
+                                <span class="fas fa-tags" style="font-size:1em;"></span>
+                                @if ($number_of_leases == 1)
+
+                                {{ $number_of_leases }} lease
+
+                                @else
+
+                                {{ $number_of_leases }} leases
+
+                                @endif
+
+                            </button>
                             @include('dnsmasq._leases_modal')
                         </li>
 
                         <li class="list-group-item">
-                            There are {{ count($config_files) }} config files.<br>
-                            <button class="btn btn-sm btn-info" data-toggle="modal" data-target="#config-files-modal">Config Files</button>
-                            @include('dnsmasq._config_files_modal')
+                            <span class="fas fa-cogs" style="font-size:1em;"></span>
+                            I am reading {{ count($config_files) }} config files when I start.
+                            <a href="#collapsable-list-of-config-files" data-toggle="collapse">Check them out</a>
+
+                            <div id="collapsable-list-of-config-files" class="row collapse">
+                                <div class="col mt-5">
+
+                                    <ul class="list-group text-dark">
+                                        @foreach($config_files as $file)
+                                            <li class="list-group-item">
+                                                <a href="/files?load={{$file}}">{{$file}}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+
+                                </div>
+                            </div>
                         </li>
 
                         <li class="list-group-item">
-                            There are {{ count($imports) }} imports.<br>
-                            <button class="btn btn-sm btn-info" data-toggle="modal" data-target="#imports-modal">Import Files</button>
-                            @include('dnsmasq._imports_modal')
+                            <span class="fas fa-box" style="font-size:1em;"></span>
+                            There are {{ count($imports) }} files available for import.
+                            <a href="#collapsable-list-of-imports" data-toggle="collapse">Check them out</a>
+
+                            <div id="collapsable-list-of-imports" class="row collapse">
+                                <div class="col mt-5">
+
+                                    <ul class="list-group text-dark">
+                                        @foreach($imports as $file)
+                                            <li class="list-group-item">
+                                                <a href="/files?load={{$file}}">{{$file}}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+
+                                </div>
+                            </div>
                         </li>
 
                     </ul>
