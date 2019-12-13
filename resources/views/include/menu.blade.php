4d9abbdff8ef9ee143ed9125009a9255c7c2d176
Goat <ryantgray@gmail.com>
2019-12-13 15:49:20 -0600

    open external links in new tabs

--- resources/views/include/menu.blade.php
+++ resources/views/include/menu.blade.php
@@ -4,9 +4,9 @@
         <a href="{{ route('dnsmasq.index') }}" class="list-group-item bg-light text-dark text-decoration-none">Dnsmasq</a>
         <a href="{{ route('recon.index') }}" class="list-group-item bg-light text-dark text-decoration-none">ReconBot</a>
         <a href="" class="list-group-item bg-secondary"></a>
-        <a href="{{ route('horizon.index') }}" class="list-group-item bg-light text-dark text-decoration-none">Horizon</a>
-        <a href="/traefik" class="list-group-item bg-light text-dark text-decoration-none">Traefik</a>
-        <a href="/dozzle" class="list-group-item bg-light text-dark text-decoration-none">Dozzle</a>
-        <a href="/portainer" class="list-group-item bg-light text-dark text-decoration-none">Portainer</a>
+        <a href="{{ route('horizon.index') }}" target="_blank" class="list-group-item bg-light text-dark text-decoration-none">Horizon</a>
+        <a href="/traefik" target="_blank" class="list-group-item bg-light text-dark text-decoration-none">Traefik</a>
+        <a href="/dozzle" target="_blank" class="list-group-item bg-light text-dark text-decoration-none">Dozzle</a>
+        <a href="/portainer" target="_blank" class="list-group-item bg-light text-dark text-decoration-none">Portainer</a>
     </ul>
 </div>
