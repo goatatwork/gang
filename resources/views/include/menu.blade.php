61578d7498098181ec36a5da6ad22fc4e8a1d89f
Goat <ryantgray@gmail.com>
2020-01-02 13:49:16 -0600

    Refactor nav menu

--- resources/views/include/menu.blade.php
+++ resources/views/include/menu.blade.php
@@ -1,12 +1,59 @@
-<div class="col-2">
-    <ul class="list-group list-group-flush">
-        <a href="{{ route('home') }}" class="list-group-item text-dark text-decoration-none">Home</a>
-        <a href="{{ route('dnsmasq.index') }}" class="list-group-item text-dark text-decoration-none">Dnsmasq</a>
-        <a href="{{ route('recon.index') }}" class="list-group-item text-dark text-decoration-none">ReconBot</a>
+<ul class="list-group list-group-flush gang-menu">
 
-        <a href="{{ route('horizon.index') }}" target="_blank" class="list-group-item text-dark text-decoration-none">Horizon</a>
-        <a href="/traefik" target="_blank" class="list-group-item text-dark text-decoration-none">Traefik</a>
-        <a href="/dozzle" target="_blank" class="list-group-item text-dark text-decoration-none">Dozzle</a>
-        <a href="/portainer" target="_blank" class="list-group-item text-dark text-decoration-none">Portainer</a>
-    </ul>
-</div>
+    <a href="{{ route('home') }}"
+        class="list-group-item text-dark text-decoration-none gang-menu-heading {{ (Request::is('/')) ? 'active' : '' }}"
+    >
+        <i class="fas fa-home"></i>
+        Home
+    </a>
+
+    <a href="#goat-collapse" data-toggle="collapse" class="list-group-item text-dark text-decoration-none gang-menu-heading {{ (Request::is('dnsmasq*')) ? 'active' : '' }}">
+        <i class="fas fa-cogs"></i>
+        Admin
+    </a>
+
+    <div id="goat-collapse" class="collapse {{ (Request::is('dnsmasq*')) ? 'show' : '' }} text-right">
+        <a href="{{ route('dnsmasq.index') }}" class="list-group-item text-dark text-decoration-none gang-menu-item">
+            Dnsmasq
+        </a>
+    </div>
+
+    <a href="{{ route('recon.index') }}"
+        class="list-group-item text-dark text-decoration-none gang-menu-heading"
+    >
+        <i class="fas fa-robot"></i>
+        ReconBot
+    </a>
+
+
+    <a href="{{ route('horizon.index') }}"
+        target="_blank"
+        class="list-group-item text-dark text-decoration-none gang-menu-heading"
+    >
+        <i class="fab fa-laravel"></i>
+        Horizon
+    </a>
+
+    <a href="/traefik"
+        target="_blank"
+        class="list-group-item text-dark text-decoration-none gang-menu-heading"
+    >
+        <!-- <img src="/traefik/dashboard/assets/images/traefik.logo.svg" height="14px" width="14px"> -->
+        <i class="fas fa-traffic-light"></i>
+        Traefik
+    </a>
+    <a href="/dozzle"
+        target="_blank"
+        class="list-group-item text-dark text-decoration-none gang-menu-heading"
+    >
+        <i class="fab fa-docker"></i>
+        Dozzle
+    </a>
+    <a href="/portainer"
+        target="_blank"
+        class="list-group-item text-dark text-decoration-none gang-menu-heading"
+    >
+        <i class="fab fa-docker"></i>
+        Portainer
+    </a>
+</ul>
