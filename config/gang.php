fa8ce01cb510c98ef069f82c07708e63f10428a8
Goat <ryantgray@gmail.com>
2020-01-04 11:15:52 -0600

    Create gang.php config file
    This also adds the corresponding entry in the .env.example file.

---
+++ config/gang.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Local Docker Daemon Information
+    |--------------------------------------------------------------------------
+    |
+    | The Docker API version provided by the docker daemon on the host
+    |
+    */
+
+    'docker_api_version' => env('GANG_DOCKER_API_VERSION', '1.40'),
+];
