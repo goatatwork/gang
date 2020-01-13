6b630b6f58d1bd443c786a2ea044affd7ef4b4eb
Goat <ryantgray@gmail.com>
2020-01-13 09:57:36 -0600

    Refactor the menu
    This is not an exhaustive list. Make right-side badges from numbers to care about. Moves external links/support components links to System menu heading.

--- resources/views/include/menu.blade.php
+++ resources/views/include/menu.blade.php
@@ -23,26 +23,72 @@
         <a href="{{ route('dnsmasq.index', 'collapsalbe-view-of-dhcp-leases') }}"
             class="list-group-item text-dark text-decoration-none gang-menu-item"
         >
-            12 Leases
+            Leases <span class="badge badge-dark text-warning">12</span>
         </a>
         <a href="{{ route('dnsmasq.index', 'collapsable-list-of-config-files') }}"
             class="list-group-item text-dark text-decoration-none gang-menu-item"
         >
-            32 Service Files
+            Service Files <span class="badge badge-dark text-warning">32</span>
         </a>
         <a href="{{ route('dnsmasq.index', 'collapsable-list-of-tftp_files') }}"
             class="list-group-item text-dark text-decoration-none gang-menu-item"
         >
-            73 TFTP Files
+            TFTP Files <span class="badge badge-dark text-warning">73</span>
         </a>
         <a href="{{ route('dnsmasq.index', 'collapsable-list-of-imports') }}"
             class="list-group-item text-dark text-decoration-none gang-menu-item"
         >
-            4 Import Files
+            Import Files <span class="badge badge-dark text-warning">4</span>
+        </a>
+    </div>
+
+
+
+
+    <!-- -->
+    <a href="#system-menu" data-toggle="collapse" class="list-group-item text-dark text-decoration-none gang-menu-heading">
+        <i class="fas fa-cogs"></i>
+        System
+    </a>
+
+    <div id="system-menu" class="collapse text-right">
+        <a href="{{ route('horizon.index') }}"
+            target="_blank"
+            class="list-group-item text-dark text-decoration-none gang-menu-item"
+        >
+            <i class="fab fa-laravel"></i>
+            Horizon
+        </a>
+
+        <a href="/traefik"
+            target="_blank"
+            class="list-group-item text-dark text-decoration-none gang-menu-item"
+        >
+            <!-- <img src="/traefik/dashboard/assets/images/traefik.logo.svg" height="14px" width="14px"> -->
+            <i class="fas fa-traffic-light"></i>
+            Traefik
+        </a>
+
+        <a href="/dozzle"
+            target="_blank"
+            class="list-group-item text-dark text-decoration-none gang-menu-item"
+        >
+            <i class="fab fa-docker"></i>
+            Dozzle
+        </a>
+        <a href="/portainer"
+            target="_blank"
+            class="list-group-item text-dark text-decoration-none gang-menu-item"
+        >
+            <i class="fab fa-docker"></i>
+            Portainer
         </a>
     </div>
 
     <!-- -->
+
+
+
 
     <a href="{{ route('recon.index') }}"
         class="list-group-item text-dark text-decoration-none gang-menu-heading"
@@ -52,34 +98,4 @@
     </a>
 
 
-    <a href="{{ route('horizon.index') }}"
-        target="_blank"
-        class="list-group-item text-dark text-decoration-none gang-menu-heading"
-    >
-        <i class="fab fa-laravel"></i>
-        Horizon
-    </a>
-
-    <a href="/traefik"
-        target="_blank"
-        class="list-group-item text-dark text-decoration-none gang-menu-heading"
-    >
-        <!-- <img src="/traefik/dashboard/assets/images/traefik.logo.svg" height="14px" width="14px"> -->
-        <i class="fas fa-traffic-light"></i>
-        Traefik
-    </a>
-    <a href="/dozzle"
-        target="_blank"
-        class="list-group-item text-dark text-decoration-none gang-menu-heading"
-    >
-        <i class="fab fa-docker"></i>
-        Dozzle
-    </a>
-    <a href="/portainer"
-        target="_blank"
-        class="list-group-item text-dark text-decoration-none gang-menu-heading"
-    >
-        <i class="fab fa-docker"></i>
-        Portainer
-    </a>
 </ul>
