4d758fbc52f1bb46d3a1c5893546b92993df292b
Goat <ryantgray@gmail.com>
2020-01-15 11:47:54 -0600

    create database table to store dhcp events in
    Messages are stored as json in a TEXT field named 'messages' and cast to an array.

--- app/DhcpEvent.php
+++ app/DhcpEvent.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class DhcpEvent extends Model
 {
-    //
+    protected $fillable = ['message','active'];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'message' => 'array'
+    ];
 }
