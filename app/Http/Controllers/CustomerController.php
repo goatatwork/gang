0c573ba8fdb16daaa7a9383b01a6f03b601410f7
Goat <ryantgray@gmail.com>
2020-02-22 12:55:09 -0600

    Paginate customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -14,7 +14,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::paginate(25);
 
         return view('provisioning.customers.index')->with('customers',$customers);
     }
