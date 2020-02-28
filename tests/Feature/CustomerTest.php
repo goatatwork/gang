687c98df1e0c4510c06a9d04336aea741749f210
Goat <ryantgray@gmail.com>
2020-02-28 12:39:43 -0600

    Add test for creating dhcp config files

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use File;
+use Storage;
 use App\Customer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -37,4 +39,31 @@ class CustomerTest extends TestCase
         $this->assertInstanceOf('Spatie\MediaLibrary\MediaCollection\MediaCollection', $customer->getMediaCollection('dhcp_configs'));
     }
 
+    /**
+     * @group customers
+     * @test
+     * @return void
+     */
+    public function test_Customer_can_create_zhone_management_dhcp_config_files()
+    {
+        // Storage::fake('local');
+        Storage::fake('customer_files');
+        $customer = factory(Customer::class)->create();
+
+        $this->assertCount(0, $customer->getMedia('dhcp_configs'));
+
+        $customer = $customer->refresh()->provisionDhcp('zhone_management','192.168.99.222','testSubScrId');
+
+        $this->assertCount(1, $customer->refresh()->getMedia('dhcp_configs'));
+
+    }
+
+    protected function renderView()
+    {
+        return view('dnsmasq.templates.zhone_management')
+            ->with('subscriber_id', 'testSubScrId')
+            ->with('customer', $customer)
+            ->with('ip', '192.168.99.222')
+            ->render();
+    }
 }
