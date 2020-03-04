091fb418a1b82fe3d5ec9451433757163c22f575
Goat <ryantgray@gmail.com>
2020-03-04 11:50:24 -0600

    Refactor customers index page to show provisioned or not provisioned

--- resources/views/provisioning/customers/index.blade.php
+++ resources/views/provisioning/customers/index.blade.php
@@ -57,28 +57,45 @@
                                 <div class="media mb-2 bg-light">
                                     <span class="fas fa-map-marker mr-3" style="font-size: 2rem;"></span>
                                     <div class="media-body">
-                                        <span class="h4 mt-0">{{$customer->name}}</span>
 
-                                        <address style="font-size:.85rem;">
-                                            <a href="#address-{{$customer->id}}"
-                                                data-toggle="collapse"
-                                                class="text-dark"
-                                                role="button"
-                                            >
-                                                [address]
-                                            </a>
+                                        <div class="row">
+                                            <div class="col">
 
-                                            <br>
+                                                <span class="h4 mt-0">{{$customer->name}}</span>
 
-                                            <span id="address-{{$customer->id}}" class="collapse show">
-                                                @if ($customer->poc_name != $customer->name) {{ $customer->poc_name }} <br> @endif
-                                                @if ($customer->address1) {{ $customer->address1 }} <br> @endif
-                                                {{ $customer->city }}, {{ $customer->state }}  {{ $customer->zip }} <br>
-                                                @if ($customer->phone1) {{ $customer->phone1 }} <br> @endif
-                                                @if ($customer->phone2) {{ $customer->phone2 }} <br> @endif
-                                                @if ($customer->poc_email) {{ $customer->poc_email }} <br> @endif
-                                            </span>
-                                        </address>
+                                                <address style="font-size:.85rem;">
+                                                    <a href="#address-{{$customer->id}}"
+                                                        data-toggle="collapse"
+                                                        class="text-dark"
+                                                        role="button"
+                                                    >
+                                                        [address]
+                                                    </a>
+
+                                                    <br>
+
+                                                    <span id="address-{{$customer->id}}" class="collapse show">
+                                                        @if ($customer->poc_name != $customer->name) {{ $customer->poc_name }} <br> @endif
+                                                        @if ($customer->address1) {{ $customer->address1 }} <br> @endif
+                                                        {{ $customer->city }}, {{ $customer->state }}  {{ $customer->zip }} <br>
+                                                        @if ($customer->phone1) {{ $customer->phone1 }} <br> @endif
+                                                        @if ($customer->phone2) {{ $customer->phone2 }} <br> @endif
+                                                        @if ($customer->poc_email) {{ $customer->poc_email }} <br> @endif
+                                                    </span>
+                                                </address>
+
+                                            </div>
+                                            <div class="col">
+                                                @if($customer->provisioned)
+                                                <span class="far fa-address-card mt-5 text-success" style="font-size: 3rem;"></span>
+                                                @else
+                                                <span class="far fa-address-card mt-5 text-secondary" style="font-size: 3rem;"></span>
+                                                @endif
+                                            </div>
+                                            <div class="col">
+
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             @endforeach
